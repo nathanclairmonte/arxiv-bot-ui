84ba5ec9aa8563a2dfaf49ccaff2a08c43c8d828
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-09 17:59:29 -0400

    added rudimentary ArxivInput functionality

--- src/components/ArxivInput/ArxivInput.js
+++ src/components/ArxivInput/ArxivInput.js
@@ -7,18 +7,63 @@ import { CgSoftwareUpload } from "react-icons/cg";
 const ArxivInput = () => {
     const [arxivId, setArxivId] = useState("");
     const [loading, setLoading] = useState(false);
-    const [statusUpdate, setStatusUpdate] = useState("testinggggg");
+    const [status, setStatus] = useState({
+        type: "neutral",
+        message: "",
+    });
 
     // handle loading of the inputted paper
     const handleLoadPaper = (event) => {
+        // prevent browser refresh
         event.preventDefault();
+
+        // if (arxivId.trim() === "") {
+        //     return;
+        // }
+
+        // clear text input and set loading to true
         setArxivId("");
         setLoading(true);
+
+        // give status update
+        setStatus({
+            type: "neutral",
+            message: `Loading https://arxiv.org/pdf/${arxivId}.pdf...`,
+        });
+
+        //load the paper into vectorstore. emulating this with a timeout
         setTimeout(_setLoadingToFalse, 2000);
     };
 
     const _setLoadingToFalse = () => {
+        // remember to move this stuff up into the handler
+        // just here right now to emulate an API call with setTimeout()
+        const data = {
+            response: {
+                type: "success",
+            },
+        };
+
+        // update status message
+        setStatus({
+            type: data.response.type,
+            message:
+                data.response.type === "success"
+                    ? "Succes! Paper loaded."
+                    : "Something went wrong :(",
+        });
         setLoading(false);
+    };
+
+    // select status styling based on message type
+    const _statusHelper = (type) => {
+        if (type === "error") {
+            return styles.status_error;
+        } else if (type === "success") {
+            return styles.status_success;
+        } else {
+            return styles.status_neutral;
+        }
     };
 
     // handle pressing of the enter key, want to load paper when this happens
@@ -61,9 +106,11 @@ const ArxivInput = () => {
                         )}
                     </button>
                 </form>
-                <div className={styles.status_container}>
-                    <p className={styles.status_text}>{statusUpdate}</p>
-                </div>
+                {status.message ? (
+                    <div className={styles.status_container}>
+                        <p className={_statusHelper(status.type)}>{status.message}</p>
+                    </div>
+                ) : null}
             </div>
         </div>
     );
