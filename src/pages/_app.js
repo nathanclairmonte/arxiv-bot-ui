8fc513e100bbcc6410d88fda6c09e3375240fbec
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-11 15:09:37 -0400

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,28 @@
+import Script from "next/script";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-J8EB9FBH5V"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-J8EB9FBH5V', {
+                            page_path: window.location.pathname,
+                        });
+                    `,
+                }}
+            />
             <Component {...pageProps} />;
             <Analytics />
         </>
